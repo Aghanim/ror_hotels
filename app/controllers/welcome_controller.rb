51f459c0f6ca4d8bb8749f00c25cb842d5fbf1f8
Aghanim <narri924@gmail.com>
2014-07-16 10:26:27 +0400

    welcome index done

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,13 +1,7 @@
 class WelcomeController < ApplicationController
   def index
-  #переделать
-    size = 5 #лимит
-    @top =Array.new()
-    n=0
-  	temp = Vote.select("hotel_id as id, sum(mark) as sum").group("hotel_id").order("sum desc").limit(size)
-  	temp.each do |i|
-  	 @top[n] = Hotel.find(i.id)
-     n=n+1
-    end
+ 
+  @hotels = Helperclass.new
+
   end
 end
